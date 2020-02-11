686e2d08ea32bf823b9bb86ce894df85960204b6
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-11 16:12:00 +0900

    Update 10. 단일행함수.sql

--- 10. 단일행함수.sql
+++ 10. 단일행함수.sql
@@ -23,18 +23,12 @@ select concat(no, concat(title, writedate)) from board;
 
 select '[' || no || ']' || title "no title" from board;
 
-select no+1000 from board;
-
-select length( max(no)) from board;
-
-select lpad(no, 4, 0) nono from board;
-
-
+-- 글자수 기준으로 자르기
 select substr('대한민국',1,2) from dual;    --대한
 select substr('대한민국',-1,2) from dual;   --국
 select substr('대한민국',2,2) from dual;    --한민
 select substr('대한민국',3,2) from dual;    --민국
-
+-- 바이트 기준으로 자르기 
 select substrb('대한민국',1,6) from dual;   --대한
 select substrb('대한민국',2,6) from dual;   -- 한
 select substrb('대한민국',3,6) from dual;   -- 한
@@ -46,8 +40,13 @@ select substrb('대한민국',7,6) from dual;   --민국
 
 select '가나다라마바사', instr('가나다라마바사' , '나다') 나다num from dual;
 
-
+-- 공지사항  제목에 <3월>  들어가는 게시글 찾기
 select no, title, content, startdate, enddate from notice
-where instr(title, '이트') > 0;
+where instr(title, '3월') > 0;
+
+
+select rpad(title,20,'-'), lpad(title,20,'-') from board;
+
+select concat(rpad(title,20,'-'), lpad(title,20,'-')) ㅋㅋㅋ from board;
 
 
