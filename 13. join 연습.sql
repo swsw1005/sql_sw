7e2c9ca2c094c92c62c6e9bc0466acb431ea33c8
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-13 17:29:10 +0900

    Update 13. join 연습.sql

--- 13. join 연습.sql
+++ 13. join 연습.sql
@@ -39,6 +39,32 @@ order by job;
 
 
 
+select * from member;
+-- 예제 message 테이블 
+create table message(
+no number primary key,
+content varchar2(2000) not null,
+sender varchar2(20) not null REFERENCES member(id),
+senddate date default sysdate,
+accepter varchar2(20) not null REFERENCES member(id),
+acceptdate date default null,
+alluser number(1) default 0);
+
+create SEQUENCE message_seq;
+
+
+insert into message (no, content, sender, accepter)
+values (   (select nvl(max(no),0) from message )+1   , '첫번째 채팅입니다. 감사합니다.', 'lee123','kim123');
+
+select * from message;
+
+
+
+-- 어려운 join  
+-- 보낸id, 보낸 이름, 받는 id, 받는 이름  4개 컬럼
+select ms.sender, sm.name, ms.accepter, am.name
+from message ms, member sm, member am
+where ms.sender = sm.id  and  ms.accepter = am.id;
 
 
 
