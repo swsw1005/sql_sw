717e9bdf8b236398ad3c446a66f5093dd53e1729
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-17 16:52:33 +0900

    Update 14. plsql.sql

--- 14. plsql.sql
+++ 14. plsql.sql
@@ -13,23 +13,73 @@ select
 from emp
 where
   empno = 7902;
-DBMS_OUTPUT.PUT_line (vno || '      ' || vname);
+DBMS_OUTPUT.PUT_line (vno || ' / ' || vname);
 end;
 /
 
+
+
+-- 게시판에서 가장 최신글 찾아서 no, title, writer, writedate출력
+/
 set serverout on;
 declare
 vno number(4);
 vtitle varchar2(50);
 vwriter varchar2(20);
+vwritedate date;
 begin
-    select no, title, writer into vno, vtitle, vwriter
+    select no, title, writer, writedate into vno, vtitle, vwriter, vwritedate
     from board
     where no = (select max(no) from board);
     
-    DBMS_output.put_line(vno || '   '|| vtitle||'  ' || vwriter);
+    DBMS_output.put_line(vno || ' / '|| vtitle||' / ' || vwriter ||' / '
+    || vwritedate);
     end;
     /
     
+    
+    
+-- 게시판에서 가장 최신글 찾아서 no, title, writer, writedate출력
+-- 날짜 형식 변경
+/
+set serverout on;
+declare
+vno number(4);
+vtitle varchar2(50);
+vwriter varchar2(20);
+vwritedate varchar2(50);--날짜 to_char이용. 스트링 변경
+begin
+    select no, title, writer, to_char(writedate,'yyyy-mm-dd HH24:mi:ss') into vno, vtitle, vwriter, vwritedate
+    from board
+    where no = (select max(no) from board);
+    
+    DBMS_output.put_line(vno || ' / '|| vtitle||' / ' || vwriter ||' / '
+    || vwritedate);
+    end;
+    /    
+    
+    
+    
+
+
+
+-- 게시판에서 가장 최신글 찾아서 no, title, writer, writedate출력
+-- &no .... no값을 직접 입력받는다.
+/
+set serverout on;
+declare
+vno board.no%type;
+vtitle board.title%type;
+vwriter board.writer%type;
+
+begin
+    select no, title, writer into vno, vtitle, vwriter
+    from board
+    where no = &no;
+    -- no값 입력받는 form 띄운다.
+    
+    DBMS_output.put_line(vno || ' / '|| vtitle||' / ' || vwriter );
+    end;
+    /
     
     ------------------
