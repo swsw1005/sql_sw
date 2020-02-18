03a8f38b67011b3085148484b6462253f25e35b2
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-18 17:09:36 +0900

    Update 15. PL SQL_loop.sql

--- 15. PL SQL_loop.sql
+++ 15. PL SQL_loop.sql
@@ -2,17 +2,54 @@
 declare
     no number := 0;-- 변수 선언 + 초기값
     
-
-
 begin
     loop
-
     dbms_output.put_line(no || '%');
     no := no+1;
     exit when no>100;
     end loop;
     dbms_output.put_line('download Complete');
     dbms_output.put_line('system off');
+end;
+/
+
+---------------------
+----------------------
+----------------------
+--loop를 이용한 반복문 처리 p599  
+--whlie loop
+declare
+    no number := 0;-- 변수 선언 + 초기값
+    
+begin
+    while no <=100 loop
+    dbms_output.put_line(no || '%');
+    no := no+1;
+    end loop;
+    dbms_output.put_line('download Complete');
+    dbms_output.put_line('system off');
 
 end;
 /
+
+
+
+---------------------
+----------------------
+----------------------
+--loop를 이용한 반복문 처리 p599  
+--for 
+-- 반복횟수 고정 >>  for 문
+-- 반복횟수 변동 >> while 문
+begin
+    for no in 150 .. 200 loop
+    dbms_output.put_line(no || '%');
+    end loop;
+    dbms_output.put_line('download Complete');
+    dbms_output.put_line('system off');
+
+end;
+/
+
+
+-
