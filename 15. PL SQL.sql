0d5eca325ddd46a08fbedba1ab8967202e850377
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-18 15:53:47 +0900

    Update 15. PL SQL.sql

--- 15. PL SQL.sql
+++ 15. PL SQL.sql
@@ -16,10 +16,84 @@ select
   ename into v_rec
 from emp
 where
-  empno = 7902;
---화면에 표시하기
-  DBMS_output.put_line('  ');
-DBMS_output.put_line(v_rec.emp_id || ' / ' || v_rec.e_name);
-DBMS_output.put_line('  ');
+  empno = & no;
+--숫자 직접 입력
+  --화면에 표시하기
+  DBMS_output.put_line(' ');
+DBMS_output.put_line(' ');
+DBMS_output.put_line(' ');
+DBMS_output.put_line('----------------------------------');
+DBMS_output.put_line(
+    v_rec.emp_id || '번...너의 이름은 ★☆★' || v_rec.e_name || '...☆★☆'
+  );
+DBMS_output.put_line('----------------------------------');
 end;
 /
+----------------------------------------------
+----------------------------------------------
+----------------------------------------------
+
+-- 데이터 중 사원번호가 7902 사원번호, 이름, 부서번호, 부서명 출력
+
+declare 
+vempno emp.empno%TYPE;
+vename  emp.ename%TYPE;
+vdeptno  emp.deptno%TYPE;
+vdeptname  varchar2(12);
+begin --데이터 가져오기
+select
+  empno,  ename,  deptno 
+  into vempno,  vename,  vdeptno
+from emp
+where
+  empno = 7902;
+ ---if ~ end if 문
+  --elsif <<< elseif 아니다!!!!
+  if(vdeptno = 10) then vdeptname := '기획부';
+elsif(vdeptno = 20) then vdeptname := '경리부';
+elsif(vdeptno = 30) then vdeptname := '영업부';
+elsif(vdeptno = 40) then vdeptname := '전산부';
+  else vdeptname := '무소속';
+end if;
+--
+-- 처리한 데이터 출력
+dbms_output.put_line(
+  vempno || ' / ' ||  vename || ' / ' ||  vdeptno  || ' / ' ||  vdeptname );
+end;
+/
+
+----------------------------------------------
+----------------------------------------------
+----------------------------------------------
+
+-- 데이터 중  사원번호, 이름, 부서번호, 부서명 출력
+
+declare 
+vempno emp.empno%TYPE;
+vename  emp.ename%TYPE;
+vdeptno  emp.deptno%TYPE;
+vdeptname  varchar2(12);
+begin --데이터 가져오기
+select
+  empno,  ename,  deptno 
+  into vempno,  vename,  vdeptno
+from emp
+where
+  empno = &empno;
+  
+  
+  vdeptname := case
+    when vdeptno = 10 then  '기획부'
+    when vdeptno = 20 then  '경리부'
+    when vdeptno = 30 then '영업부'
+    when vdeptno = 40 then '전산부'
+end ;
+--
+-- 처리한 데이터 출력
+dbms_output.put_line(
+  vempno || ' / ' ||  vename || ' / ' ||  vdeptno  || ' / ' ||  vdeptname );
+end;
+/
+
+
+---
