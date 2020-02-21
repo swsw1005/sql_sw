b3e19124544b4794222050fb47293f4ce4ec8b1b
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2020-02-21 17:33:32 +0900

    Update 01. 사용자 생성,제거,권한부여,비밀번호 변경.sql

--- 01. 사용자 생성,제거,권한부여,비밀번호 변경.sql
+++ 01. 사용자 생성,제거,권한부여,비밀번호 변경.sql
@@ -2,9 +2,9 @@
 --1. 사용자 생성
 create user java00 identified by java00;
 --2. 사용자에게 권한을 부여해서 작업을 할수 있도록 해줘야 한다.
-GRANT CONNECT, RESOURCE TO java00;
+GRANT CONNECT,
+RESOURCE TO java00;
 --3. 사용자 비밀번호 바꾸기
 alter user sys identified by q1w2e3;
-
 --4. 사용자 제거
 Drop User java00 Cascade;
